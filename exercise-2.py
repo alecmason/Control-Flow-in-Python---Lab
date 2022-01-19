05d16219772db7c460fe8a6fba80cee5feec8dce
Alec Jones <alecjones@Alecs-MacBook-Air.local>
2022-01-19 17:00:32 -0600

    ex 2 completed

--- exercise-2.py
+++ exercise-2.py
@@ -7,3 +7,8 @@
 #      - What you entered is xx characters long
 # 3. Return to step 1, unless the word 'quit' was entered.
 
+phrase = input("Please enter a word or phrase: ").lower()
+
+while (phrase != 'quit'):
+    print(len(phrase))
+    phrase = input("Please enter a word or phrase: ").lower()
