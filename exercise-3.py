1d583b72e504d972eeacc5aaf2d82312370629e0
Alec Jones <alecjones@Alecs-MacBook-Air.local>
2022-01-19 17:18:13 -0600

    ex3 finito

--- exercise-3.py
+++ exercise-3.py
@@ -10,3 +10,12 @@
 #      The dog's age in dog years is xx
 
 # Hint:  Use the int() function to convert the string returned from input() into an integer
+
+human_years = int(input("Input a dog's age in human years: "))
+
+if (human_years <= 2):
+    dog_years = human_years * 10
+    print(f"The dog's age in dog years is {dog_years}")
+else: 
+    dog_years = 20 + (human_years - 2) * 7
+    print(f"The dog's age in dog years is {dog_years}")
